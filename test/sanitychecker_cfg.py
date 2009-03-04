1a221693e3e45df72040c1956c32b7c2e15c7a72
jmmaes <>
2009-03-04 08:42:53 +0000

    Solved issue with too much output of the TopDecayChain module. By setting a limit on the category name of the edm::LogVerbatim to zero the output is fully suppressed

--- test/sanitychecker_cfg.py
+++ test/sanitychecker_cfg.py
@@ -2,29 +2,34 @@ import FWCore.ParameterSet.Config as cms
   
 process = cms.Process("SanityCheck")
 
-#Message Logger
-
+#Message Logger (more info see: https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideMessageLogger)
 process.MessageLogger = cms.Service("MessageLogger",
-    # 3 files for 3 different type of output
-    destinations = cms.untracked.vstring("Warning_SC", "ErrorSummary_SC", "InfoSummary_SC"),
-    debugModules = cms.untracked.vstring('*'),#for all modules
-    categories = cms.untracked.vstring("NoDataFound","LinkBroken","SummaryError","MainResults"), # list of categories
-    #Warning_SC = cms.untracked.PSet( threshold = cms.untracked.string('DEBUG'), categories = cms.untracked.vstring("cat") ),
+   
+    destinations = cms.untracked.vstring("Warning_SC", "ErrorSummary_SC", "InfoSummary_SC"), # 3 files for 3 different type of output
+
+    debugModules = cms.untracked.vstring('*'), #for all modules
+
+    categories = cms.untracked.vstring("inputChain","decayChain","NoDataFound","LinkBroken","SummaryError","MainResults"), # list of categories
+                                                                                                                           # inputChain and decayChain come from the TopDecaySubsetModule and need to be suppressedin warning- and info-summary
+
     Warning_SC = cms.untracked.PSet( threshold = cms.untracked.string("DEBUG"), 
                                      NoDataFound = cms.untracked.PSet(reportEvery = cms.untracked.int32(10)), 
-                                     LinkBroken = cms.untracked.PSet(reportEvery = cms.untracked.int32(2)) 
+                                     LinkBroken = cms.untracked.PSet(reportEvery = cms.untracked.int32(2)),
+                                     inputChain = cms.untracked.PSet(limit = cms.untracked.int32(0)),
+                                     decayChain = cms.untracked.PSet(limit = cms.untracked.int32(0))
 				     ),
     ErrorSummary_SC = cms.untracked.PSet( threshold = cms.untracked.string("ERROR"), categories = cms.untracked.vstring("SummaryError") ),
-    InfoSummary_SC = cms.untracked.PSet( threshold = cms.untracked.string("INFO"), categories = cms.untracked.vstring("MainResults") )
+    InfoSummary_SC = cms.untracked.PSet( threshold = cms.untracked.string("INFO"), categories = cms.untracked.vstring("MainResults"),
+                                         inputChain = cms.untracked.PSet(limit = cms.untracked.int32(0)),
+                                         decayChain = cms.untracked.PSet(limit = cms.untracked.int32(0)))
 )
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
-
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        'file:myfile.root'
+        #'file:myfile.root'
     )
 )
 
