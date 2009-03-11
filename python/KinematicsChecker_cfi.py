a1b2b33ef8e2769b54da836924bb17df7193019e
jmmaes <>
2009-03-11 19:41:21 +0000

    added verbose_

--- python/KinematicsChecker_cfi.py
+++ python/KinematicsChecker_cfi.py
@@ -4,10 +4,11 @@ kinematics = cms.EDAnalyzer('KinematicsChecker',
 	jetsName        = cms.InputTag('selectedLayer1Jets'),
 	muonsName       = cms.InputTag('selectedLayer1Muons'),
         metsName        = cms.InputTag('selectedLayer1METs'),
-        matchingAlgo    = cms.int32(0),
+        matchingAlgo    = cms.int32(2),
         useMaxDist      = cms.bool(True),
-        useDeltaR       = cms.bool(False),
-        maxDist         = cms.double(0.5),
+        useDeltaR       = cms.bool(True),
+        maxDist         = cms.double(0.3),
         jetsAcceptance  = cms.vdouble(2.4,15),                 #acceptance region for jets in jet collection   (eta,pt)
-        muonsAcceptance  = cms.vdouble(2.5,15)                 #acceptance region for muons in muon collection (eta,pt)
+        muonsAcceptance = cms.vdouble(2.5,15),                 #acceptance region for muons in muon collection (eta,pt)
+        verbose         = cms.bool(False)
 ) 
