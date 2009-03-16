0a7c6a0a76b828cc998a177fcb3e2cf19ef21e3d
villella <>
2009-03-16 15:47:08 +0000

    Plots and log infos added

--- src/VertexChecker.cc
+++ src/VertexChecker.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  local user
 //         Created:  Wed Feb 18 16:39:03 CET 2009
-// $Id: VertexChecker.cc,v 1.1 2009/03/06 13:37:15 villella Exp $
+// $Id: VertexChecker.cc,v 1.2 2009/03/09 15:08:19 jmmaes Exp $
 //
 //
 
@@ -42,6 +42,7 @@
 
 #include "TDirectory.h"
 #include "TH1D.h"
+#include "TH2D.h"
 #include "TH1F.h"
 #include "TH2F.h"
 
@@ -65,9 +66,10 @@ class VertexChecker : public edm::EDAnalyzer {
    
   //Histograms are booked in the beginJob() method
   std::map<std::string,TDirectory*> TDirectorycontainer_; // simple map to contain all TDirectory.
-  std::map<std::string,TH1D*> TH1Dcontainer_; // simple map to contain all TH1D.
-  std::map<std::string,TH1F*> TH1Fcontainer_; // simple map to contain all TH1F.
-  std::map<std::string,TH2F*> TH2Fcontainer_; // simple map to contain all TH2F.
+  std::map<std::string,TH1D*> TH1Dcontainer_;       // simple map to contain all TH1D.
+  std::map<std::string,TH2D*> TH2Dcontainer_;       // simple map to contain all TH2D.
+  std::map<std::string,TH1F*> TH1Fcontainer_;       // simple map to contain all TH1F.
+  std::map<std::string,TH2F*> TH2Fcontainer_;       // simple map to contain all TH2F.
 };
 
 //
@@ -135,8 +137,11 @@ VertexChecker::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    double sumpt2_vtx =0;
    double ptAssTrk = -100;
    int prim_vtxidx = -2;
+   int sec_vtxidx = -2;
    double sumpt =0;
    double sumpt_prim =0;
+   double pt2_primvtx =  0;
+   double sumpt2_primvtx =0;
    //  Primary Vertex infos;
    if(vertex->size() > 0){
      TH1Dcontainer_["VtxNumber"]->Fill(vertex->size());
@@ -145,7 +150,7 @@ VertexChecker::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
       TH1Dcontainer_["Vrt_x"]->Fill((*vertex)[j].x());
       TH1Dcontainer_["Vrt_y"]->Fill((*vertex)[j].y());
       TH1Dcontainer_["Vrt_z"]->Fill((*vertex)[j].z());
-
+      TH2Dcontainer_["Vrt_xy"]->Fill((*vertex)[j].x(),(*vertex)[j].y() );
        TH1Dcontainer_["VrtTrkNumber"]->Fill((*vertex)[j].tracksSize());
        reco::Vertex::trackRef_iterator tr ;  
        for ( tr = (*vertex)[j].tracks_begin(); tr !=(*vertex)[j].tracks_end(); tr++){
@@ -158,17 +163,23 @@ VertexChecker::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 	 ptAssTrk = sumpt2_vtx;
 	 prim_vtxidx =j; 
       }
-     }//close for vertex
+       //      if(j !=prim_vtxidx) sec_vtxidx =j;
+      }//close for vertex
 	 TH1Dcontainer_["PrimaryVrt_x"]->Fill((*vertex)[ prim_vtxidx].x());
 	 TH1Dcontainer_["PrimaryVrt_y"]->Fill((*vertex)[ prim_vtxidx].y());
 	 TH1Dcontainer_["PrimaryVrt_z"]->Fill((*vertex)[ prim_vtxidx].z());   
 	 TH1Dcontainer_["VrtTrkSumPt"]->Fill(sumpt);
+	 TH2Dcontainer_["PrimaryVrt_xy"]->Fill((*vertex)[ prim_vtxidx].x(), (*vertex)[ prim_vtxidx].y());
+	 TH1Dcontainer_["PrimaryVrtTrkNumber"]->Fill((*vertex)[prim_vtxidx].tracksSize());
        reco::Vertex::trackRef_iterator trp ;  
     for ( trp = (*vertex)[ prim_vtxidx].tracks_begin(); trp !=(*vertex)[ prim_vtxidx].tracks_end(); trp++){
 	 sumpt_prim += (*trp)->pt();
+	 pt2_primvtx =  (*trp)->pt() * (*trp)->pt() ;
+	 sumpt2_primvtx += pt2_primvtx;
     }
 	 TH1Dcontainer_["PrimVrtTrkSumPt"]->Fill(sumpt_prim);
-   }
+	 TH1Dcontainer_["PrimVrtTrkSumPt2"]->Fill(sumpt2_primvtx);
+   }//close if
    
  
    //tree levels of message
@@ -194,10 +205,10 @@ VertexChecker::beginJob(const edm::EventSetup&)
 {
   edm::Service<TFileService> fs;
   if (!fs) throw edm::Exception(edm::errors::Configuration, "TFileService missing from configuration!");
-
-   TFileDirectory subDir = fs->mkdir( "Vertex" );
-   // TFileDirectory subsubDir = subDir.mkdir( "Vertex" );
-
+  
+  TFileDirectory subDir = fs->mkdir( "Vertex" );
+  // TFileDirectory subsubDir = subDir.mkdir( "Vertex" );
+  
   //define the histograms booked
   //TH1D
   
@@ -205,14 +216,17 @@ VertexChecker::beginJob(const edm::EventSetup&)
   TH1Dcontainer_["Vrt_x"]= subDir.make<TH1D>("Vtx_x" ,"vertices x ",50,-1, 1);
   TH1Dcontainer_["Vrt_y"]= subDir.make<TH1D>("Vtx_y" ,"vertices y ",50,-1, 1);
   TH1Dcontainer_["Vrt_z"]= subDir.make<TH1D>("Vtx_z" ,"vertices z ",100,-10, 10);
+  TH2Dcontainer_["Vrt_xy"]= subDir.make<TH2D>("Vtx_xy" ,"vertices x vs y ",50,-1, 1, 50, -1, 1);
   TH1Dcontainer_["PrimaryVrt_x"]= subDir.make<TH1D>("PrimaryVtx_x" ,"Primary vertex x ",50,-1, 1);
   TH1Dcontainer_["PrimaryVrt_y"]= subDir.make<TH1D>("PrimaryVtx_y" ,"Primary vertex y ",50,-1, 1);
   TH1Dcontainer_["PrimaryVrt_z"]= subDir.make<TH1D>("PrimaryVtx_z" ,"Primary vertex z ",100,-10, 10);
+  TH2Dcontainer_["PrimaryVrt_xy"] =  subDir.make<TH2D>("PrimaryVtx_xy" ,"Primary vertex x vs y ",50,-1, 1, 50, -1, 1);
+  TH1Dcontainer_["PrimaryVrtTrkNumber"]= subDir.make<TH1D>("PrimaryVrtTrkNumber","Number of tracks associated to primary vertex",10,0, 10);
   TH1Dcontainer_["VrtTrkNumber"] = subDir.make<TH1D>("VrtTrkNumber" ,"Number of tracks associated to vertices ",10,0, 10);
   TH1Dcontainer_["VrtTrkPt"] = subDir.make<TH1D>("VrtTrkPt" ,"track associated to vertices Pt ",100,0, 50);
   TH1Dcontainer_["VrtTrkSumPt"]= subDir.make<TH1D>("VrtTrkSumPt" ," Sum of the pt of the tracks associated to vertices",100,0, 300);
- TH1Dcontainer_["PrimVrtTrkSumPt"]= subDir.make<TH1D>("PrimVrtTrkSumPt" ," Sum of the pt of the tracks associated to Primary vertex",50,0, 300);
-
+  TH1Dcontainer_["PrimVrtTrkSumPt"]= subDir.make<TH1D>("PrimVrtTrkSumPt" ," Sum of the pt of the tracks associated to Primary vertex",50,0, 300);
+  TH1Dcontainer_["PrimVrtTrkSumPt2"]= subDir.make<TH1D>("PrimVrtTrkSumPt2" ,"Sum of the pt^2 of the tracks associated to Primary vertex",50,0, 10000);
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
@@ -221,7 +235,22 @@ VertexChecker::endJob() {
    //use LogError to summarise the error that happen in the execution (by example from warning) (ex: Nof where we cannot access such variable)
    //edm::LogError  ("SummaryError") << "My error message \n";    // or  edm::LogProblem  (not formated)
    //use LogInfo to summarise information (ex: pourcentage of events matched ...)
-   //edm::LogInfo   ("MainResults") << "My LogInfo message \n";  // or  edm::LogVerbatim (not formated)
+
+  edm::LogVerbatim ("MainResults") << " -------------------------------------------";
+  edm::LogVerbatim ("MainResults") << " -------------------------------------------";
+  edm::LogVerbatim ("MainResults") << " --     Report from Vertex Checker       -- ";
+  edm::LogVerbatim ("MainResults") << " -------------------------------------------";
+  edm::LogVerbatim ("MainResults") << " -------------------------------------------";
+  edm::LogVerbatim ("MainResults") << "Mean Number of Vertices  = " << TH1Dcontainer_["VtxNumber"]->GetMean() ;  
+  edm::LogVerbatim ("MainResults") << "Vertices Mean x = " << TH1Dcontainer_["Vrt_x"]->GetMean() ;  
+  edm::LogVerbatim ("MainResults") << "Vertices Mean y = " << TH1Dcontainer_["Vrt_y"]->GetMean() ;  
+  edm::LogVerbatim ("MainResults") << "Vertices Mean z = " << TH1Dcontainer_["Vrt_z"]->GetMean() ;  
+  edm::LogVerbatim ("MainResults") << " -------------------------------------------";
+  edm::LogVerbatim ("MainResults") << " -----    Primary Vertex Info  -------------";
+  edm::LogVerbatim ("MainResults") << "Prim Vertex Mean x = " << TH1Dcontainer_["PrimaryVrt_x"]->GetMean() ;  
+  edm::LogVerbatim ("MainResults") << "Prim Vertex Mean y = " << TH1Dcontainer_["PrimaryVrt_y"]->GetMean() ;  
+  edm::LogVerbatim ("MainResults") << "Prim Vertex Mean z = " << TH1Dcontainer_["PrimaryVrt_z"]->GetMean() ;  
+
 }
 
 //define this as a plug-in
