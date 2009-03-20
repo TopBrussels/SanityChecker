1e848243d5ed0d8064920bc9439ffe6628e4a5d4
jmmaes <>
2009-03-20 19:24:42 +0000

    - added categories and suppres them - changed the dependency of modules on each other

--- test/sanitychecker_cfg.py
+++ test/sanitychecker_cfg.py
@@ -6,22 +6,30 @@ process = cms.Process("SanityCheck")
 process.MessageLogger = cms.Service("MessageLogger",
    
     destinations = cms.untracked.vstring("Warning_SC", "ErrorSummary_SC", "InfoSummary_SC"), # 3 files for 3 different type of output
-    statistics = cms.untracked.vstring("statistics.txt"),
+    statistics = cms.untracked.vstring("Statistics_SC"),
     debugModules = cms.untracked.vstring('*'), #for all modules
 
-    categories = cms.untracked.vstring("inputChain","decayChain","NoDataFound","LinkBroken","SummaryError","MainResults"), # list of categories
-                                                                                                                           # inputChain and decayChain come from the TopDecaySubsetModule and need to be suppressedin warning- and info-summary
+    categories = cms.untracked.vstring("inputChain","decayChain","NoDataFound","LinkBroken_noJetsFound","LinkBroken_noMuonsFound","LinkBroken_noMetsFound","LinkBroken_noGenEvtFound","LinkBroken_ISRJets","LinkBroken_TopRadJets","SummaryError","MainResults"), # list of categories
+                                                                                                  # inputChain and decayChain come from the TopDecaySubsetModule and need to be suppressedin warning- and info-summary
 
     Warning_SC = cms.untracked.PSet( threshold = cms.untracked.string("DEBUG"), 
                                      NoDataFound = cms.untracked.PSet(reportEvery = cms.untracked.int32(10)), 
-                                     LinkBroken = cms.untracked.PSet(reportEvery = cms.untracked.int32(2)),
+                                     #LinkBroken = cms.untracked.PSet(reportEvery = cms.untracked.int32(2)),
+                                     LinkBroken = cms.untracked.PSet(limit = cms.untracked.int32(5)),
+                                     LinkBroken_noGenEvt = cms.untracked.PSet(limit = cms.untracked.int32(5)),
+                                     LinkBroken_ISRJets = cms.untracked.PSet(limit = cms.untracked.int32(5)),
+                                     LinkBroken_TopRadJets = cms.untracked.PSet(limit = cms.untracked.int32(5)),
                                      inputChain = cms.untracked.PSet(limit = cms.untracked.int32(0)),
                                      decayChain = cms.untracked.PSet(limit = cms.untracked.int32(0))
 				     ),
     ErrorSummary_SC = cms.untracked.PSet( threshold = cms.untracked.string("ERROR"), categories = cms.untracked.vstring("SummaryError") ),
     InfoSummary_SC = cms.untracked.PSet( threshold = cms.untracked.string("INFO"), categories = cms.untracked.vstring("MainResults"),
+                                         LinkBroken_noGenEvt = cms.untracked.PSet(limit = cms.untracked.int32(5)),
+                                         LinkBroken_ISRJets = cms.untracked.PSet(limit = cms.untracked.int32(5)),
+                                         LinkBroken_TopRadJets = cms.untracked.PSet(limit = cms.untracked.int32(5)),
                                          inputChain = cms.untracked.PSet(limit = cms.untracked.int32(0)),
-                                         decayChain = cms.untracked.PSet(limit = cms.untracked.int32(0)))
+                                         decayChain = cms.untracked.PSet(limit = cms.untracked.int32(0))),
+    #Statistics_SC = cms.untracked.PSet(threshold = cms.untracked.string("DEBUG"))
 )
 
 process.load("TopBrussels.SanityChecker.PATLayer1_Ttjets_MG_input_cfi")
@@ -45,17 +53,17 @@ process.TFileService = cms.Service("TFileService",
         fileName = cms.string('SanityChecker.root')
 	)
 	
-
 process.p = cms.Path(
-    process.makeGenEvt *
-    (process.TtGenEventChecker +
-     process.Resolutions_lJets +
-     process.Resolutions_bJets +
-     process.jetmet +
-     process.muonchecker +
-     (process.ttDecaySelection * process.TruthReco) +
-     process.vertex) +
-     process.kinematics 
-    )
-
+    process.makeGenEvt  
+    + process.TtGenEventChecker 
+    + process.Resolutions_lJets 
+    + process.Resolutions_bJets 
+    + process.jetmet 
+    + process.muonchecker 
+    + process.vertex 
+    + process.jetmet 
+    + process.kinematics 
+    + (process.ttDecaySelection + process.TruthReco) # This line should be commented in case of not running on ttbar events
+                                                     # because the ttDecaySelection will throw an edm exception thrown if no genevent is there
+)
 
